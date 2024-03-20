b798cf0cbf269503ffd943382b57165ada0ab2b3
adam.nowak <adam.nowak@microsoft.wsei.edu.pl>
2024-03-20 13:36:38 +0100

    Add project files.

---
+++ Web/Pages/Quiz/Summary.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.AccessControl;
+
+namespace BackendLab01.Pages;
+
+public class Summary : PageModel
+{
+    public int CorrectAnswersCount { get; set; }
+
+    private readonly IQuizUserService _userService;
+
+    public Summary(IQuizUserService userService)
+    {
+        _userService= userService;
+    }
+
+    public void OnGet(int quizId, int userId)
+    {
+        CorrectAnswersCount = _userService.CountCorrectAnswersForQuizFilledByUser(quizId, userId);
+    }
+}
